80df37bcf9323836d5e7315887854ab5c957777c
Kristof-nl <hermaszewski.krzysztof@gmail.com>
2022-04-27 19:46:44 +0200

    Home anf Not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { CollectionsModule } from './collections/collections.module';
 import { ElementsModule } from './elements/elements.module';
 import { ViewsModule } from './views/views.module';
 import { ModsModule } from './mods/mods.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ElementsModule,
     CollectionsModule,
     ViewsModule,
-    ModsModule
+    ModsModule,
+    AppRoutingModule
 
   ],
   providers: [],
