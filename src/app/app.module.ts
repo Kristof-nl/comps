0eacbe7c92abf6c6333ef07304fa534aab6c21cc
Kristof-nl <hermaszewski.krzysztof@gmail.com>
2022-04-28 20:27:10 +0200

    Lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CollectionsModule } from './collections/collections.module';
-import { ElementsModule } from './elements/elements.module';
 import { ViewsModule } from './views/views.module';
 import { ModsModule } from './mods/mods.module';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +17,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    ElementsModule,
     CollectionsModule,
     ViewsModule,
     ModsModule,
