822c3f741ca3115a07e29675a44b10214457ca10
Kristof-nl <hermaszewski.krzysztof@gmail.com>
2022-04-30 12:44:35 +0200

    Added placeholder component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CollectionsModule } from './collections/collections.module';
 import { ViewsModule } from './views/views.module';
 import { ModsModule } from './mods/mods.module';
 import { HomeComponent } from './home/home.component';
@@ -17,7 +16,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    CollectionsModule,
     ViewsModule,
     ModsModule,
     AppRoutingModule
